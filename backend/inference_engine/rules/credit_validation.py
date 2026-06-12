22f7073041bd0045bc62fba614072ba17a6eaed2
Inaurrara <inaurrararozado.aluno@unipampa.edu.br>
2026-06-12 19:43:11 -0300

    chore(inference-engine): alinha modulo de regras ao padrao da development

--- backend/inference_engine/rules/credit_validation.py
+++ backend/inference_engine/rules/credit_validation.py
@@ -14,37 +14,42 @@ Responsabilidades:
 - Casos de teste: creditos_validos, basico_insuficiente, tecnologico_excedido,
   total_insuficiente.
 """
+
+from __future__ import annotations
+
 from inference_engine.terms import Compound, Variable
 
+_A = Variable("A")
+_P = Variable("P")  # Único programa: os 4 fatos de configuração compartilham o mesmo P.
+_N1, _N2, _N3, _T = Variable("N1"), Variable("N2"), Variable("N3"), Variable("T")
+_MIN1, _MIN2, _MAX3, _MINT = (
+    Variable("Min1"),
+    Variable("Min2"),
+    Variable("Max3"),
+    Variable("MinT"),
+)
 
-def register(rule_base) -> None:
-    aluno = Variable("Aluno")
-    programa = Variable("Programa")
-
-    basico = Variable("Basico")
-    especifico = Variable("Especifico")
-    tecnologico = Variable("Tecnologico")
-    total = Variable("Total")
-
-    min_basico = Variable("MinBasico")
-    min_especifico = Variable("MinEspecifico")
-    max_tecnologico = Variable("MaxTecnologico")
-    min_total = Variable("MinTotal")
-
-    rule_base.add_rule(
-        Compound("creditos_validos", [aluno]),
+# creditos_validos(A) :-
+#   creditos_grupo_basico(A, N1),      min_creditos_basico(P, Min1),     gte(N1, Min1),
+#   creditos_grupo_especifico(A, N2),  min_creditos_especifico(P, Min2), gte(N2, Min2),
+#   creditos_grupo_tecnologico(A, N3), max_creditos_tecnologico(P, Max3), lte(N3, Max3),
+#   total_creditos(A, T),              min_creditos_total(P, MinT),       gte(T, MinT).
+CLAUSES: list[tuple[Compound, list[Compound]]] = [
+    (
+        Compound("creditos_validos", [_A]),
         [
-            Compound("creditos_grupo_basico", [aluno, basico]),
-            Compound("creditos_grupo_especifico", [aluno, especifico]),
-            Compound("creditos_grupo_tecnologico", [aluno, tecnologico]),
-            Compound("total_creditos", [aluno, total]),
-            Compound("min_creditos_basico", [programa, min_basico]),
-            Compound("min_creditos_especifico", [programa, min_especifico]),
-            Compound("max_creditos_tecnologico", [programa, max_tecnologico]),
-            Compound("min_creditos_total", [programa, min_total]),
-            Compound("gte", [basico, min_basico]),
-            Compound("gte", [especifico, min_especifico]),
-            Compound("lte", [tecnologico, max_tecnologico]),
-            Compound("gte", [total, min_total]),
+            Compound("creditos_grupo_basico", [_A, _N1]),
+            Compound("min_creditos_basico", [_P, _MIN1]),
+            Compound("gte", [_N1, _MIN1]),
+            Compound("creditos_grupo_especifico", [_A, _N2]),
+            Compound("min_creditos_especifico", [_P, _MIN2]),
+            Compound("gte", [_N2, _MIN2]),
+            Compound("creditos_grupo_tecnologico", [_A, _N3]),
+            Compound("max_creditos_tecnologico", [_P, _MAX3]),
+            Compound("lte", [_N3, _MAX3]),
+            Compound("total_creditos", [_A, _T]),
+            Compound("min_creditos_total", [_P, _MINT]),
+            Compound("gte", [_T, _MINT]),
         ],
-    )
+    ),
+]
